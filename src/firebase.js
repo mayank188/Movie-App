14e01177bcb8e8e5307b1c136671af2635a6f0b6
mayank188 <mayanksom36@gmail.com>
2026-05-07 23:32:14 +0530

    add

---
+++ src/firebase.js
@@ -0,0 +1,20 @@
+// Firebase config and exports for Movie-App
+import { initializeApp } from "firebase/app";
+import { getAuth, GoogleAuthProvider } from "firebase/auth";
+import { getFirestore } from "firebase/firestore";
+
+const firebaseConfig = {
+  apiKey: "YOUR_API_KEY",
+  authDomain: "YOUR_AUTH_DOMAIN",
+  projectId: "YOUR_PROJECT_ID",
+  storageBucket: "YOUR_STORAGE_BUCKET",
+  messagingSenderId: "YOUR_MESSAGING_SENDER_ID",
+  appId: "YOUR_APP_ID"
+};
+
+const app = initializeApp(firebaseConfig);
+const auth = getAuth(app);
+const firestore = getFirestore(app);
+const googleProvider = new GoogleAuthProvider();
+
+export { auth, firestore, googleProvider };
